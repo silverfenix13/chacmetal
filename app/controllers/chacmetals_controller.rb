d5bcb3d50156760b968c77792756ec8ec336b14e
silverfenix13 <scourge.arcano.13@gmail.com>
2015-01-15 13:39:51 -0600

    nuevo chacmetal

---
+++ app/controllers/chacmetals_controller.rb
@@ -0,0 +1,25 @@
+class ChacmetalsController < ApplicationController
+  def index
+
+  end
+
+  def show
+  	
+  end
+
+  def create
+  	
+  end
+
+  def update
+  	
+  end
+
+  def edit
+  	
+  end
+
+  def new
+  	
+  end
+end
